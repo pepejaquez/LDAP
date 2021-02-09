bff92eb3e0ec7e97205d9adadff49f957ffd6594
pepe jaquez <therealpepejaquez@gmail.com>
2021-02-09 15:25:11 -0800

    initial push of source files for LDAP.

---
+++ DatabaseConnection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDAPProperties
+{
+    class DatabaseConnection
+    {
+        
+        string connString = "Provider=Microsoft.ACE.OLEDB.12.0;data source=T:\\Laptops\\Laptop Database.accdb";
+
+        public Dictionary<string, bool> studentKeys = new Dictionary<string, bool>();
+
+        public OleDbConnection dbconn;
+        public OleDbDataAdapter dataAdapter;
+        public DataTable dataTable;       
+        public DataSet ds;
+        public int currRec = 0;
+        public int totalRec = 0;
+
+        public DatabaseConnection()
+        {
+            GetDatabaseData();
+            InitializeCommands();
+        }
+
+        public void GetDatabaseData()
+        {
+            // Retrieve the student id's from the database to use as keys in the studentKeys dictionary.
+            dbconn = new OleDbConnection(connString);
+            string commandString = "SELECT * from tblStudents";
+            dataAdapter = new OleDbDataAdapter(commandString, dbconn);
+            ds = new DataSet();
+            dataAdapter.Fill(ds, "tblStudents");
+
+            dataTable = ds.Tables["tblStudents"];
+            currRec = 0;
+            totalRec = dataTable.Rows.Count;
+
+            foreach(DataRow row in dataTable.Rows)
+            {
+                object data = row["id"];
+                string strData = data.ToString();
+                studentKeys.Add(strData, true);
+            }            
+        }  
+
+        public void InitializeCommands()
+        {
+            dataAdapter.InsertCommand = dbconn.CreateCommand();
+            dataAdapter.InsertCommand.CommandText = "INSERT INTO tblStudents(id, lastName, firstName) VALUES(@id, @lastName, @firstName)";
+            AddParams(dataAdapter.InsertCommand, "id", "lastName", "firstName");
+        }
+
+        private void AddParams(OleDbCommand cmd, params string[] cols)
+        {
+            foreach(string col in cols)
+            {
+                cmd.Parameters.Add("@" + col, OleDbType.Char, 0, col);
+            }
+        }
+    }
+}
---
+++ LDAP.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDAPProperties
+{
+    class LDAP
+    {
+
+        DirectoryEntry de = new DirectoryEntry("LDAP://pdc1/OU=Students,dc=pencol,dc=local");
+
+        // Additions to this list are the children of 'OU=Students' (e.g. "OU=CSE,OU=Students")
+        public List<string> DirectoryChildren = new List<string>(){ "OU=Students" };
+
+        string serverString = "LDAP://pdc1/";
+        string location = ",dc=pencol,dc=local";
+
+        public DataRow row;
+
+
+        public LDAP()
+        {
+            // Populate the DirectoryChildren List with the 'OU' entries of OU=Students.
+            foreach (DirectoryEntry entry in de.Children)
+            {
+                if (entry.Name.ToString().Substring(0, 2) == "OU")
+                {
+                    DirectoryChildren.Add(entry.Name + ",OU=Students");
+                }
+            }
+        }
+        
+        public void GetNewStudents(Dictionary<string, bool> dict, DataTable dt, OleDbDataAdapter da, DataSet ds)
+        {
+            foreach (string s in DirectoryChildren)
+            {
+                DirectoryEntry de = new DirectoryEntry(serverString + s + location);
+
+                foreach (DirectoryEntry entry in de.Children)
+                {
+                    if (!(entry.Properties["cn"].Value == null) && entry.Properties["cn"].Value.ToString()[0] == 's')
+                    {
+                        // Add the entries that do not already have their key in the dictionary.
+                        if (!dict.ContainsKey(entry.Properties["cn"].Value.ToString().Substring(1, 6)))
+                        {
+                            row = dt.NewRow();
+                            row.BeginEdit();
+                            int totalRec = dt.Rows.Count;
+                            int currRec = totalRec - 1;
+
+                            // Strip the 's' off the id.
+                            row["id"] = entry.Properties["cn"].Value.ToString().Substring(1, 6);
+
+                            // Change the names to propercase.
+                            row["lastName"] = ProperCase(entry.Properties["sn"].Value.ToString().ToLower());
+                            row["firstName"] = ProperCase(entry.Properties["givenName"].Value.ToString().ToLower());
+                            dt.Rows.Add(row);
+                        }
+                    }
+                }                
+            }
+            da.Update(ds, "tblStudents");
+        }
+
+        private string ProperCase(string str)
+        {
+            StringBuilder sb = new StringBuilder(str);
+
+            // Change the first character to uppercase.
+            sb[0] = char.ToUpper(sb[0]);
+
+            // Change the third character to uppercase. (e.g. Mccoy is now McCoy)
+            if (str.Substring(0, 2) == "mc")
+            {                  
+                sb[2] = char.ToUpper(sb[2]);
+            }
+
+            // Change the character after the space to uppercase. (e.g. Bickle biggs is now Bickle Biggs)
+            if (str.Contains(' '))
+            {
+                sb[str.IndexOf(' ') + 1] = char.ToUpper(sb[str.IndexOf(' ') + 1]);
+            }
+
+            // Change the character after the hyphen to uppercase. (e.g. Graham-harvey is now Graham-Harvey)
+            if (str.Contains('-'))
+            {
+                sb[str.IndexOf('-') + 1] = char.ToUpper(sb[str.IndexOf('-') + 1]);
+            }
+            
+            return sb.ToString();
+        }        
+    }
+}
